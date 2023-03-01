0a8854b9db38fbf31d0f968ae16b8cadf01687f3
LucasMelll0 <lucasmellorodrigues2012@gmail.com>
2023-03-01 13:10:35 -0300

    fix: Ao requisitar os mapas em pt-BR o uuid não vinha.

--- src/main/java/com/valorantwiki/valorant_maps/model/MapPt.java
+++ src/main/java/com/valorantwiki/valorant_maps/model/MapPt.java
@@ -1,13 +1,10 @@
 package com.valorantwiki.valorant_maps.model;
 
 import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 
 @Entity
 @Table(name = "maps_br")
 public class MapPt extends Map {
 
-    @Id
-    private String uuid;
 }
